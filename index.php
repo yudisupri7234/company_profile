3d2d2ed6f96fdf4feb1e9fdc754455fdca2f854c
yudisupri7234 <yudisupri723@gmail.com>
2022-11-21 17:11:17 +0700

    update 21-11-2022 [16.35]

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <title>UNIVERSITAS MUHAMMADIYAH BANTEN | LANDING PAGE</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <!-- Main header -->
+        <section class="header">
+            <?php include 'header.php'?>
+        </section>
+        <!-- Main content -->
+        <section class="jumbotron">
+            <?php include 'jumbotron.php' ?>
+            
+        </section>
+        <!-- Main content -->
+        <section class="main">
+            <?php include 'main.php'?>
+        </section>
+        <section>
+            
+        </section>
+        <!-- Footer -->
+        <Section class="Footer">
+            <?php include 'footer.php' ?>
+        </Section>
+    </div>
+</body>
+</html>
